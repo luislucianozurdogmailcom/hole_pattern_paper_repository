3cbc716ecc4d51c8916a853c24f468d709536c14
Luciano Zurdo <luciano.zurdo@educacion.local>
2022-10-20 23:34:34 -0300

    Cambiamos la columna 4 a frecuencia angular para los gráficos

--- Programas y datos/salida_csv.py
+++ Programas y datos/salida_csv.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import math
 
 """
 Programa para borrar los indices y los nombres de las columnas en la data
@@ -17,6 +18,11 @@ for i in os.listdir():
         if 'Unnamed: 0' in df.columns:
             # Tiramos a la basura la columna de indices hecha antes
             df = df.drop(columns = ['Unnamed: 0']); 
-        
+            
+            # Cambiamos la columna a frec adim * 2pi
+            df['Frec Adim'] = df['Frec Adim'] * 2 * math.pi;
+
+            # Printeo por motivos de testing
+            print(df['Frec Adim'])
         # Exportamos a CSV
         df.to_csv(i[:-4]+'csv',index = False, header = False)
